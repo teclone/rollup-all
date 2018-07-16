c510e2ff97473224a1fa71b753d61cfe7af38c78
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-16 10:23:57 +0100

    feat: add a getEntryPath method that inspects the run path and extracts the project root directory
    The project root directory is needed in order to resolve all other paths accurately

--- src/modules/Bundler.js
+++ src/modules/Bundler.js
@@ -23,4 +23,42 @@ export default class Bundler {
     get [Symbol.toStringTag]() {
         return 'Bundler';
     }
+
+    /**
+     * returns the entry path
+    */
+    getEntryPath(mainFileName) {
+        if (mainFileName.indexOf('node_modules') > 0)
+            return mainFileName.split('/node_modules')[0];
+
+        let currentPath = path.join(mainFileName, '../'),
+        rightPath = '';
+        while (currentPath !== '/') {
+            if (fs.existsSync(currentPath + '/package.json')) {
+                rightPath = currentPath;
+                break;
+            }
+            currentPath = path.join(currentPath, '../');
+        }
+        return rightPath;
+    }
+
+    /**
+     * runs the process
+     *@returns {Array}
+    */
+    process() {
+        //resolve user defined settings
+        let entryPath = this.getEntryPath(require.main.filename),
+        config = null;
+
+        if (fs.existsSync(entryPath + '/' + this.configPath))
+            config = Object.assign({}, _config, require(entryPath + '/' + this.configPath));
+        else
+            config = _config;
+
+        //extract lib and dist configs
+        let libConfig = config.libConfig,
+        distConfig = config.distConfig;
+    }
 }
