0b3a0ff415ff52c4f796b74f83b0c941a4f67588
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-09-16 17:52:10 +0200

    fix: improve file matching and filtering

--- src/modules/Bundler.ts
+++ src/modules/Bundler.ts
@@ -1,223 +1,231 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import {copy, pickValue, isString, camelCase} from '@forensic-js/utils';
+import { copy, pickValue, isString, camelCase } from '@forensic-js/utils';
 import { Config, UserConfig, CommonConfig, LibConfig, DistConfig, Module, Build } from '../@types';
 import defualtConfig from '../.buildrc';
 import { COMMON_CONFIGS } from '../Constants';
-import {mkDirSync, isProdEnv, getEntryPath} from '@forensic-js/node-utils';
+import { mkDirSync, isProdEnv, getEntryPath } from '@forensic-js/node-utils';
 
 export default class Bundler {
+  private plugins: object[] = [];
 
-    private plugins: object[] = [];
+  private pluginsWithUglifier: object[] = [];
 
-    private pluginsWithUglifier: object[] = [];
+  private entryPath: string = '';
 
-    private entryPath: string = '';
+  private config: Config = defualtConfig;
 
-    private config: Config = defualtConfig;
+  /**
+   * @param uglifierPlugin uglifier plugin if uglify settings is enabled
+   * @param otherPlugins array of other plugins
+   * @param config path to user defined build config or the user defined config object
+   */
+  constructor(uglifierPlugin: object | null, otherPlugins: object[], config: string | UserConfig) {
+    this.plugins = otherPlugins;
+    this.pluginsWithUglifier = [...otherPlugins];
 
-
-    /**
-     * @param uglifierPlugin uglifier plugin if uglify settings is enabled
-     * @param otherPlugins array of other plugins
-     * @param config path to user defined build config or the user defined config object
-     */
-    constructor(uglifierPlugin: object | null, otherPlugins: object[], config: string | UserConfig) {
-        this.plugins = otherPlugins;
-        this.pluginsWithUglifier = [...otherPlugins];
-
-        if (uglifierPlugin) {
-            this.pluginsWithUglifier.push(uglifierPlugin);
-        }
-
-        this.entryPath = getEntryPath();
-        this.config = this.resolveConfig(this.entryPath, config);
+    if (uglifierPlugin) {
+      this.pluginsWithUglifier.push(uglifierPlugin);
     }
 
-    /**
-     * resolves the pattern into a regex object
-     *@param {Array|string|RegExp} patterns - array of patterns or string pattern
-     *@param {Array} regexStore - array to store regex objects
-    */
-    private resolveRegex(patterns: (string|RegExp)[]) {
-        return patterns.map(pattern => {
-            if (isString(pattern)) {
-                if (pattern === '*') {
-                    return new RegExp('.*');
-                }
-                else {
-                    pattern = pattern.replace(/\./g, '\\.').replace(/\*{2}/g, '.*')
-                        .replace(/\*/g, '[^/]+');
-                    return new RegExp(pattern, 'i');
-                }
-            }
-            else {
-                return pattern;
-            }
+    this.entryPath = getEntryPath();
+    this.config = this.resolveConfig(this.entryPath, config);
+  }
+
+  /**
+   * resolves the pattern into a regex object
+   *@param {Array|string|RegExp} patterns - array of patterns or string pattern
+   *@param {Array} regexStore - array to store regex objects
+   */
+  private resolveRegex(patterns: (string | RegExp)[]) {
+    return patterns.map(pattern => {
+      if (isString(pattern)) {
+        if (pattern === '*') {
+          return new RegExp('.*');
+        } else {
+          pattern = pattern
+            .replace(/\./g, '\\.')
+            .replace(/\*{2}/g, '.*')
+            .replace(/\*/g, '[^/]+');
+          return new RegExp(pattern, 'i');
+        }
+      } else {
+        return pattern;
+      }
+    });
+  }
+
+  /**
+   * pick config from target config or global config
+   * @param prop config property
+   * @param targetConfig target config
+   * @param config global config
+   */
+  private pickConfig(prop: keyof CommonConfig, targetConfig: LibConfig | DistConfig, config: Config) {
+    if (prop !== 'include' && prop !== 'exclude') {
+      //@ts-ignore
+      targetConfig[prop] = pickValue(prop, targetConfig, config[prop]);
+    } else {
+      targetConfig[prop] = this.resolveRegex(pickValue(prop, targetConfig, config[prop]));
+    }
+  }
+
+  /**
+   * resolves the config object
+   */
+  private resolveConfig(entryPath: string, config: string | UserConfig): Config {
+    if (isString(config)) {
+      const absPath = path.resolve(entryPath, config);
+      try {
+        config = require(absPath) as UserConfig;
+      } catch (ex) {
+        console.info(`Proceeding with default config options. "${config}" did not match any file`);
+        config = {};
+      }
+    }
+    const resolvedConfig: Config = copy({}, defualtConfig, config as Config);
+
+    //resolve lib config
+    COMMON_CONFIGS.forEach(prop => {
+      this.pickConfig(prop, resolvedConfig.libConfig, resolvedConfig);
+      this.pickConfig(prop, resolvedConfig.distConfig, resolvedConfig);
+    });
+
+    //enable output compression if running in production environment
+    if (isProdEnv()) {
+      resolvedConfig.libConfig.uglify = true;
+      resolvedConfig.distConfig.uglify = true;
+    }
+    return resolvedConfig;
+  }
+
+  /**
+   * copies the file from the src to the destination
+   */
+  private copyFile(src: string, filePath: string) {
+    const dest = path.resolve(this.entryPath, filePath);
+    mkDirSync(dest);
+
+    fs.copyFileSync(src, dest);
+  }
+
+  /**
+   * generates build export for each module
+   */
+  private getExports(exportStore: Build[], modules: Module[], config: DistConfig | LibConfig) {
+    let src = null;
+    const regexMatches = regex => regex.test(src);
+
+    for (const { isAsset, filePath, oldRelativePath, newRelativePath, isTypeDefinitionFile, name } of modules) {
+      src = filePath;
+      if (isAsset && config.copyAssets) {
+        this.copyFile(filePath, path.join(config.outDir, oldRelativePath));
+      } else if (isTypeDefinitionFile && config.copyTypings) {
+        this.copyFile(filePath, path.join(config.typingsDir, oldRelativePath));
+      } else if (
+        !isAsset &&
+        !isTypeDefinitionFile &&
+        config.include.some(regexMatches) &&
+        (config.exclude.length === 0 || !config.exclude.some(regexMatches))
+      ) {
+        exportStore.push({
+          input: filePath,
+          output: {
+            file: path.resolve(this.entryPath, config.outDir, newRelativePath),
+            format: config.format,
+            name: camelCase(name),
+            interop: config.interop,
+            sourcemap: config.sourcemap,
+            globals: this.config.globals,
+          },
+          plugins: config.uglify ? this.pluginsWithUglifier : this.plugins,
+          external: () => {
+            return config.format === 'cjs';
+          },
+          watch: this.config.watch,
         });
+      }
     }
+  }
 
-    /**
-     * pick config from target config or global config
-     * @param prop config property
-     * @param targetConfig target config
-     * @param config global config
-     */
-    private pickConfig(prop: keyof CommonConfig, targetConfig: LibConfig | DistConfig,
-        config: Config) {
-        if (prop !== 'include' && prop !== 'exclude') {
-            targetConfig[prop] = pickValue(prop, targetConfig, config[prop]);
-        }
-        else {
-            targetConfig[prop] = this.resolveRegex(pickValue(prop, targetConfig, config[prop]));
-        }
-    }
-
-    /**
-     * resolves the config object
-     */
-    private resolveConfig(entryPath: string , config: string | UserConfig): Config {
-        if (isString(config)) {
-            const absPath = path.resolve(entryPath, config);
-            try {
-                config = require(absPath) as UserConfig;
-            }
-            catch(ex){
-                console.info(`Proceeding with default config options. "${config}" did not match any file`);
-                config = {};
-            }
-        }
-        const resolvedConfig: Config = copy({}, defualtConfig, config as Config);
-
-        //resolve lib config
-        COMMON_CONFIGS.forEach(prop => {
-            this.pickConfig(prop, resolvedConfig.libConfig, resolvedConfig);
-            this.pickConfig(prop, resolvedConfig.distConfig, resolvedConfig);
-        });
-
-        //enable output compression if running in production environment
-        if (isProdEnv()) {
-            resolvedConfig.libConfig.uglify = true;
-            resolvedConfig.distConfig.uglify = true;
-        }
-        return resolvedConfig;
-    }
-
-    /**
-     * copies the file from the src to the destination
-    */
-    private copyFile(src: string, filePath: string) {
-        const dest = path.resolve(this.entryPath, filePath);
-        mkDirSync(dest);
-
-        fs.copyFileSync(src, dest);
-    }
-
-    /**
-     * generates build export for each module
-    */
-    private getExports(exportStore: Build[], modules: Module[], config: DistConfig | LibConfig) {
-        let src = null;
-        const regexMatches = function (regex) {
-            return regex.test(src);
-        };
-
-        for (const {isAsset, filePath, oldRelativePath, newRelativePath, isTypeDefinitionFile, name} of modules) {
-            src = filePath;
-            if (isAsset && config.copyAssets) {
-                this.copyFile(filePath, path.join(config.outDir, oldRelativePath));
-            }
-            else if (isTypeDefinitionFile && config.copyTypings) {
-                this.copyFile(filePath, path.join(config.typingsDir, oldRelativePath));
-            }
-            else if (!isAsset && !isTypeDefinitionFile && config.include.some(regexMatches) &&
-                (config.exclude.length === 0 || !config.exclude.some(regexMatches))) {
-                exportStore.push({
-                    input: filePath,
-                    output: {
-                        file: path.resolve(this.entryPath, config.outDir, newRelativePath),
-                        format: config.format,
-                        name: camelCase(name),
-                        interop: config.interop,
-                        sourcemap: config.sourcemap,
-                        globals: this.config.globals
-                    },
-                    plugins: config.uglify? this.pluginsWithUglifier : this.plugins,
-                    external: () => {
-                        return config.format === 'cjs';
-                    },
-                    watch: this.config.watch
-                });
-            }
-        }
-    }
-
-    /**
-     * gets all modules
-    */
-    private getModules(modules: Module[], resolvedPath: string, mainModuleFileName: string,
-        mainModuleName: string, currentRelativeDir: string, fileExtensions: string[]) {
-
-        const files = fs.readdirSync(resolvedPath);
-        for (const file of files) {
-
-            const filePath = path.join(resolvedPath, file);
-            if (fs.statSync(filePath).isDirectory()) {
-                this.getModules(
-                    modules, filePath, mainModuleFileName, mainModuleName,
-                    path.join(currentRelativeDir, file), fileExtensions
-                );
-            }
-            else {
-                const extname = path.extname(file);
-                const baseName = path.basename(file, extname);
-
-                const tdTester = /\.d\.ts$/i; //type definition tester
-
-                const isAsset = !fileExtensions.includes(extname) && !tdTester.test(file);
-                const isTypeDefinitionFile = tdTester.test(file);
-
-                const oldRelativePath = path.join(currentRelativeDir, file);
-                const newRelativePath = path.join(currentRelativeDir, baseName + '.js');
-
-                modules.push({
-                    oldRelativePath,
-                    newRelativePath,
-                    isAsset,
-                    isTypeDefinitionFile,
-                    filePath,
-                    name: oldRelativePath === mainModuleFileName? mainModuleName : baseName
-                });
-            }
-        }
-        return modules;
-    }
-
-    /**
-     * returns the resolved config object
-     */
-    getConfig() {
-        return this.config;
-    }
-
-    /**
-     * runs the process
-    */
-    process() {
-
-        const startAt = path.resolve(this.entryPath, this.config.srcDir);
-        const config = this.config;
-        const modules = this.getModules(
-            [], startAt, config.mainModuleFileName, config.mainModuleName, '', config.fileExtensions
+  /**
+   * gets all modules
+   */
+  private getModules(
+    modules: Module[],
+    resolvedPath: string,
+    mainModuleFileName: string,
+    mainModuleName: string,
+    currentRelativeDir: string,
+    fileExtensions: string[]
+  ) {
+    const files = fs.readdirSync(resolvedPath);
+    for (const file of files) {
+      const filePath = path.join(resolvedPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        this.getModules(
+          modules,
+          filePath,
+          mainModuleFileName,
+          mainModuleName,
+          path.join(currentRelativeDir, file),
+          fileExtensions
         );
+      } else {
+        const firstDotPos = file.charAt(0) === '.' ? file.indexOf('.', 1) : file.indexOf('.');
+        const baseName = firstDotPos > -1 ? file.substring(0, firstDotPos) : file;
+        const extname = firstDotPos > -1 ? file.substring(firstDotPos) : '';
 
-        const exportStore: Build[] = [];
-        if (config.libConfig.enabled) {
-            this.getExports(exportStore, modules, config.libConfig);
-        }
-        if (config.distConfig.enabled) {
-            this.getExports(exportStore, modules, config.distConfig);
-        }
-        return exportStore;
+        const tdTester = /\.d\.ts$/i; //type definition tester
+
+        const isAsset = !fileExtensions.includes(extname) && !tdTester.test(file);
+        const isTypeDefinitionFile = tdTester.test(file);
+
+        const oldRelativePath = path.join(currentRelativeDir, file);
+        const newRelativePath = path.join(currentRelativeDir, baseName + '.js');
+
+        modules.push({
+          oldRelativePath,
+          newRelativePath,
+          isAsset,
+          isTypeDefinitionFile,
+          filePath,
+          name: oldRelativePath === mainModuleFileName ? mainModuleName : baseName,
+        });
+      }
     }
+    return modules;
+  }
+
+  /**
+   * returns the resolved config object
+   */
+  getConfig() {
+    return this.config;
+  }
+
+  /**
+   * runs the process
+   */
+  process() {
+    const startAt = path.resolve(this.entryPath, this.config.srcDir);
+    const config = this.config;
+    const modules = this.getModules(
+      [],
+      startAt,
+      config.mainModuleFileName,
+      config.mainModuleName,
+      '',
+      config.fileExtensions
+    );
+
+    const exportStore: Build[] = [];
+    if (config.libConfig.enabled) {
+      this.getExports(exportStore, modules, config.libConfig);
+    }
+    if (config.distConfig.enabled) {
+      this.getExports(exportStore, modules, config.distConfig);
+    }
+    return exportStore;
+  }
 }
--- tests/modules/Bundler.spec.ts
+++ tests/modules/Bundler.spec.ts
@@ -4,110 +4,107 @@ import path from 'path';
 import rimraf from 'rimraf';
 import { UserConfig } from '../../src/@types';
 
-describe('Bundler', function () {
-    const rootDir = path.resolve(__dirname, '../../');
-    let bundler: Bundler = null;
+describe('Bundler', function() {
+  const rootDir = path.resolve(__dirname, '../../');
+  let bundler: Bundler = null;
 
-    beforeEach(function() {
-        bundler = new Bundler(null, [], '.buildrc.js');
+  beforeEach(function() {
+    bundler = new Bundler(null, [], '.buildrc.js');
+  });
+
+  afterEach(function() {
+    rimraf.sync(path.resolve(__dirname, '../../dist'));
+    rimraf.sync(path.resolve(__dirname, '../../lib'));
+  });
+
+  describe(`#constructor(uglifierPlugin: object | null, otherPlugins: object[], configPath: string)`, function() {
+    it(`should create a bundler instance when called`, function() {
+      expect(bundler).toBeInstanceOf(Bundler);
     });
 
-    afterEach(function () {
-        rimraf.sync(path.resolve(__dirname, '../../dist'));
-        rimraf.sync(path.resolve(__dirname, '../../lib'));
+    it(`can take a rollup uglifier plugin object as first parameter`, function() {
+      expect(new Bundler(uglify(), [], '')).toBeInstanceOf(Bundler);
     });
 
-    describe(`#constructor(uglifierPlugin: object | null, otherPlugins: object[], configPath: string)`, function () {
-        it(`should create a bundler instance when called`, function () {
-            expect(bundler).toBeInstanceOf(Bundler);
-        });
-
-        it(`can take a rollup uglifier plugin object as first parameter`, function () {
-            expect(new Bundler(uglify(), [], '')).toBeInstanceOf(Bundler);
-        });
-
-        it(`should resolve config object and set the uglify option to true if running in
+    it(`should resolve config object and set the uglify option to true if running in
             production mode`, function() {
-            process.env.NODE_ENV = 'prod';
-            const bundler = new Bundler(null, [], '');
+      process.env.NODE_ENV = 'prod';
+      const bundler = new Bundler(null, [], '');
 
-            expect(bundler.getConfig().libConfig.uglify).toBeTruthy();
-            expect(bundler.getConfig().distConfig.uglify).toBeTruthy();
+      expect(bundler.getConfig().libConfig.uglify).toBeTruthy();
+      expect(bundler.getConfig().distConfig.uglify).toBeTruthy();
 
-            process.env.NODE_ENV = '';
-        });
-
-        it(`should resolve config object exclude and include parameters to regex objects`, function() {
-            const userConfig: UserConfig = {
-                libConfig: {
-                    include: [
-                        '*',
-                        'src/*',
-                        /src\/*/
-                    ]
-                }
-            };
-            const bundler = new Bundler(null, [], userConfig);
-            expect(bundler.getConfig().libConfig.include[0]).toEqual(/.*/);
-            expect(bundler.getConfig().libConfig.include[1]).toEqual(/src\/[^\/]+/i);
-        });
+      process.env.NODE_ENV = '';
     });
 
-    describe('#process()', function () {
-        it(`should return array of module build objects for the enabled builds`, function () {
-            process.env.NODE_ENV = 'prod';
-            let builds = bundler.process();
-            expect(builds.length).toEqual(4);
-
-            expect(typeof builds[0].external()).toEqual('boolean');
-            expect(builds).toBeInstanceOf(Array);
-
-            const bundler2 = new Bundler(null, [], {
-                distConfig: {
-                    enabled: true,
-                    uglify: true
-                }
-            });
-            builds = bundler2.process();
-            expect(builds.length).toEqual(8);
-        });
-
-        it(`should include return array of module build objects for the enabled builds`, function () {
-            const builds = bundler.process();
-            expect(typeof builds[0].external()).toEqual('boolean');
-            expect(builds).toBeInstanceOf(Array);
-        });
-
-        it(`should return empty array if no build is enabled`, function () {
-            const bundler = new Bundler(null, [], {
-                libConfig: {
-                    enabled: false
-                },
-                distConfig: {
-                    enabled: false
-                }
-            });
-            const builds = bundler.process();
-            expect(builds.length).toEqual(0);
-        });
-
-        it(`should return empty array if the include option specifies no matching files or if
-        the exclude option excludes all files`, function () {
-            const bundler = new Bundler(null, [], {
-                libConfig: {
-                    include: []
-                }
-            });
-            const builds = bundler.process();
-            expect(builds.length).toEqual(0);
-
-            const bundler2 = new Bundler(null, [], {
-                libConfig: {
-                    exclude: ['*']
-                }
-            });
-            const builds2 = bundler2.process();
-            expect(builds2.length).toEqual(0);
-        });
+    it(`should resolve config object exclude and include parameters to regex objects`, function() {
+      const userConfig: UserConfig = {
+        libConfig: {
+          include: ['*', 'src/*', /src\/*/],
+        },
+      };
+      const bundler = new Bundler(null, [], userConfig);
+      expect(bundler.getConfig().libConfig.include[0]).toEqual(/.*/);
+      expect(bundler.getConfig().libConfig.include[1]).toEqual(/src\/[^\/]+/i);
     });
+  });
+
+  describe('#process()', function() {
+    it(`should return array of module build objects for the enabled builds`, function() {
+      process.env.NODE_ENV = 'prod';
+      let builds = bundler.process();
+      console.log(builds);
+      expect(builds.length).toEqual(4);
+
+      expect(typeof builds[0].external()).toEqual('boolean');
+      expect(builds).toBeInstanceOf(Array);
+
+      const bundler2 = new Bundler(null, [], {
+        distConfig: {
+          enabled: true,
+          uglify: true,
+        },
+      });
+      builds = bundler2.process();
+      expect(builds.length).toEqual(8);
+    });
+
+    it(`should include return array of module build objects for the enabled builds`, function() {
+      const builds = bundler.process();
+      expect(typeof builds[0].external()).toEqual('boolean');
+      expect(builds).toBeInstanceOf(Array);
+    });
+
+    it(`should return empty array if no build is enabled`, function() {
+      const bundler = new Bundler(null, [], {
+        libConfig: {
+          enabled: false,
+        },
+        distConfig: {
+          enabled: false,
+        },
+      });
+      const builds = bundler.process();
+      expect(builds.length).toEqual(0);
+    });
+
+    it(`should return empty array if the include option specifies no matching files or if
+        the exclude option excludes all files`, function() {
+      const bundler = new Bundler(null, [], {
+        libConfig: {
+          include: [],
+        },
+      });
+      const builds = bundler.process();
+      expect(builds.length).toEqual(0);
+
+      const bundler2 = new Bundler(null, [], {
+        libConfig: {
+          exclude: ['*'],
+        },
+      });
+      const builds2 = bundler2.process();
+      expect(builds2.length).toEqual(0);
+    });
+  });
 });
