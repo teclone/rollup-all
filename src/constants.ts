c361eec4efbc76549eba0a68070a7019a6a944bc
Harrison Ifeanyichukwu <harrison.ifeanyichukwu.u@gmail.com>
2025-07-31 09:57:48 +0200

    feat: exclude test snapshot files in build outputs

--- src/constants.ts
+++ src/constants.ts
@@ -13,3 +13,6 @@ export const BASE_ASSET_EXTENSIONS = [
 ];
 
 export const formats: BuildFormat[] = ['cjs', 'es', 'iife', 'umd'];
+
+export const EXCLUDE_FILES_EXT_REGEX =
+  /\.(spec|test|stories|tests|specs|cy|snap)(\.[\w-_]+)*$/i;
